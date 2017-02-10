734a065601fe436dcc0c3e9e99a13bfc0e0a2cba
soedomoto <soedomoto@gmail.com>
2017-02-10 20:15:45 +0700

    Add JNI method

--- jni/com_soedomoto_vrp_solver_CoESVRPJNI.cpp
+++ jni/com_soedomoto_vrp_solver_CoESVRPJNI.cpp
@@ -2,16 +2,6 @@
 #include <community.hpp>
 #include "com_soedomoto_vrp_solver_CoESVRPJNI.h"
 
-jint vSize;
-jfloat* jvXDepots;
-jfloat* jvYDepots;
-jfloat* jvDurations;
-jint* jvCapacities;
-jint cSize;
-jfloat* jcXDepots;
-jfloat* jcYDepots;
-jint* jcDemands;
-
 JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_setVehicles
         (JNIEnv *env, jobject, jint size, jfloatArray xDepots, jfloatArray yDepots, jfloatArray durations, jintArray capacities) {
     vSize = size;
@@ -31,12 +21,121 @@ JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_setCustomers
     jcDemands = env->GetIntArrayElements(demands ,0);
 }
 
+JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_setCustomerToCustomerDistance
+        (JNIEnv *env, jobject, jint sizeX, jint sizeY, jobjectArray distances) {
+    customerDistances = typedef_vectorMatrix<float>(sizeX);
+    for (int i = 0; i < sizeX; ++i) customerDistances.at(i).resize(sizeY);
+
+    for (int i = 0; i < sizeX; ++i) {
+        jfloatArray rows = (jfloatArray) env->GetObjectArrayElement(distances, i);
+        jfloat *row = env->GetFloatArrayElements(rows, 0);
+        for (int j = 0; j < sizeY; ++j) {
+            customerDistances.at(i).at(j) = row[j];
+        }
+    }
+
+    explicitC2CDistance = true;
+}
+
+JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_setDepotToCustomerDistance
+        (JNIEnv *env, jobject, jint sizeX, jint sizeY, jobjectArray distances) {
+    depotDistances = typedef_vectorMatrix<float>(sizeX);
+    for (int i = 0; i < sizeX; ++i) depotDistances.at(i).resize(sizeY);
+
+    for (int i = 0; i < sizeX; ++i) {
+        jfloatArray rows = (jfloatArray) env->GetObjectArrayElement(distances, i);
+        jfloat *row = env->GetFloatArrayElements(rows, 0);
+        for (int j = 0; j < sizeY; ++j) {
+            depotDistances.at(i).at(j) = row[j];
+        }
+    }
+
+    explicitD2CDistance = true;
+}
+
+JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_configSetNumSubpopulation
+        (JNIEnv *, jobject, jint num) {
+    config->setNumSubIndDepots(num);
+}
+
+JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_configStopWhenMaxExecutionTime
+        (JNIEnv *, jobject, jfloat time) {
+    config->setExecutionTime(time);
+}
+
+JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_configStopWhenMaxTimeWithoutUpdate
+        (JNIEnv *, jobject, jfloat time) {
+    config->setMaxTimeWithoutUpdate(time);
+}
+
+JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_configStopWhenNumGeneration
+        (JNIEnv *, jobject, jint gen) {
+    config->setNumGen(gen);
+}
+
+JNIEXPORT jintArray JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_getSolutionDepots
+        (JNIEnv *env, jobject) {
+    jintArray jArr = env->NewIntArray(depots.size());
+    env->SetIntArrayRegion(jArr, 0, depots.size(), &depots[0]);
+    depots.empty();
+
+    return jArr;
+}
+
+JNIEXPORT jintArray JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_getSolutionRoutes
+        (JNIEnv *env, jobject) {
+    jintArray jArr = env->NewIntArray(routes.size());
+    env->SetIntArrayRegion(jArr, 0, routes.size(), &routes[0]);
+    routes.empty();
+
+    return jArr;
+}
+
+JNIEXPORT jfloatArray JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_getSolutionCosts
+        (JNIEnv *env, jobject) {
+    jfloatArray jArr = env->NewFloatArray(costs.size());
+    env->SetFloatArrayRegion(jArr, 0, costs.size(), &costs[0]);
+    costs.empty();
+
+    return jArr;
+}
+
+JNIEXPORT jintArray JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_getSolutionDemands
+        (JNIEnv *env, jobject) {
+    jintArray jArr = env->NewIntArray(demands.size());
+    env->SetIntArrayRegion(jArr, 0, demands.size(), &demands[0]);
+    demands.empty();
+
+    return jArr;
+}
+
+JNIEXPORT jobjectArray JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_getSolutionCustomers
+        (JNIEnv *env, jobject) {
+    int yDim = customers.size();
+    int xDim = 0;
+    for_each(customers.begin(), customers.end(), [&](vector<int> c) {
+        if (c.size() > xDim) xDim = c.size();
+    });
+
+    jintArray inner = env->NewIntArray(xDim);
+    jobjectArray outer = env->NewObjectArray(yDim, env->GetObjectClass(inner), 0);
+
+    for (std::size_t i = 0; i < yDim; ++i) {
+        env->SetIntArrayRegion(inner, 0, customers.at(i).size(), &customers.at(i)[0]);
+        env->SetObjectArrayElement(outer, i, inner);
+
+        if (i + 1 != yDim) {
+            inner = env->NewIntArray(xDim);
+        }
+    }
+
+    customers.empty();
+
+    return outer;
+}
+
 JNIEXPORT jint JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_solve
         (JNIEnv *env, jobject) {
-    MDVRPProblem *problem = new MDVRPProblem();
-    AlgorithmConfig *config = new AlgorithmConfig();
-
-    // Solver *solver = new Solver();
     problem->setInstCode("java");
     problem->setInstance("java");
     problem->setDepots(vSize);
@@ -47,9 +146,6 @@ JNIEXPORT jint JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_solve
     problem->setBestKnowSolution(1.0);
 
     for (int i = 0; i < problem->getDepots(); ++i) {
-//        problem->setDuration(jvDurations[i]);
-//        problem->setCapacity(jvCapacities[i]);
-
         problem->getDurations().at(i) = jvDurations[i];
         problem->getCapacities().at(i) = jvCapacities[i];
 
@@ -70,6 +166,33 @@ JNIEXPORT jint JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_solve
     }
 
     problem->calculateMatrixDistance();
+    if (explicitC2CDistance) {
+        problem->setCustomerDistances(customerDistances);
+
+        double totalC2CDistance = 0.0;
+        for (int i = 0; i < problem->getCustomers(); ++i) {
+            for (int j = 0; j < problem->getCustomers(); ++j) {
+                if (i != j) {
+                    totalC2CDistance += problem->getCustomerDistances().at(i).at(j);
+                }
+            }
+        }
+        problem->setAvgCustomerDistance(
+                totalC2CDistance / (double) (problem->getCustomers() * problem->getCustomers()));
+    }
+
+    if (explicitD2CDistance) {
+        problem->setDepotDistances(depotDistances);
+
+        double totalD2CDistance = 0.0;
+        for (int i = 0; i < problem->getDepots(); ++i) {
+            for (int j = 0; j < problem->getCustomers(); ++j) {
+                totalD2CDistance += problem->getDepotDistances().at(i).at(j);
+            }
+        }
+        problem->setAvgDepotDistance(totalD2CDistance / (double) (problem->getCustomers() * 1.0));
+    }
+
     problem->setNearestCustomersFromCustomer();
     problem->setNearestCustomersFromDepot();
     problem->setNearestDepotsFromCustomer();
@@ -83,22 +206,13 @@ JNIEXPORT jint JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_solve
     printf("=======================================================================================\n");
     printf("CoES -- A Cooperative Coevolutionary Algorithm for Multi-Depot Vehicle Routing Problems\n");
 
-    if (config->getProcessType() == Enum_Process_Type::MONO_THREAD) printf("* MONO");
-    else printf("*** MULTI");
-
-    printf(" Thread version\n");
-    printf("=======================================================================================\n\n");
-
-    if (config->isDebug()) {
-        printf("D\tE\tB\tU\tG\tversion!\n\n");
-        printf("=======================================================================================\n\n");
-    }
-
     printf("Instance....: %s\n", problem->getInstance().c_str());
     printf("Depots......: %d\n", problem->getDepots());
     printf("Vehicles....: %d\n", problem->getVehicles());
     printf("Capacity....: %d\n", problem->getCapacities().at(0));
     printf("Duration....: %.2f\n", problem->getDurations().at(0));
+    printf("C2C Avg Dist: %.2f\n", problem->getAvgCustomerDistance());
+    printf("D2C Avg Dist: %.2f\n", problem->getAvgDepotDistance());
     printf("Customers...: %d\n", problem->getCustomers());
 
     if (config->getStopCriteria() == NUM_GER)
@@ -141,27 +255,38 @@ JNIEXPORT jint JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_solve
     community->printEvolution();
     community->getProblem()->getMonitor().updateGeneration();
 
-    // ##### Start manager ###### ----------------
+    // Search solution
     try {
         community->manager();
-    }
-    catch (exception &e) {
+    } catch (exception &e) {
         cout << e.what();
     }
-    // ########################## ----------------
 
-    // Print final solution
-    community->getEliteGroup()->getBest().printSolution();
+    // Solutions
+    int numRoutes = 0;
+    IndividualsGroup &best = community->getEliteGroup()->getBest();
+    best.printSolution();
 
-    cout << "\nCustomers: " << community->getEliteGroup()->getBest().getNumTotalCustomers() << endl;
-    cout << "Route Customers: " << community->getEliteGroup()->getBest().getNumTotalCustomersFromRoutes() << endl
-         << endl;
+    for_each(best.getIndividuals().begin(), best.getIndividuals().end(), [&numRoutes](Individual &individual) {
+        numRoutes += individual.getNumVehicles();
+    });
 
-    community->getEliteGroup()->printValues();
+    for_each(best.getIndividuals().begin(), best.getIndividuals().end(), [&](Individual &individual) {
+        individual.updateRoutesID();
+        for_each(individual.getRoutes().begin(), individual.getRoutes().end(), [&](Route &route) {
+            depots.push_back(route.getDepot());
+            routes.push_back(route.getId());
+            costs.push_back(route.getTotalCost());
+            demands.push_back(route.getDemand());
 
-    //community->printSubpopList();
+            vector<int> vcustomers;
+            for_each(route.getTour().begin(), route.getTour().end(), [&vcustomers](int customer) {
+                vcustomers.push_back(customer);
+            });
+            customers.push_back(vcustomers);
+        });
+    });
 
-    // Clear memory
     delete community;
 
     return 0;
--- jni/com_soedomoto_vrp_solver_CoESVRPJNI.h
+++ jni/com_soedomoto_vrp_solver_CoESVRPJNI.h
@@ -2,6 +2,30 @@
 #include <jni.h>
 /* Header for class com_soedomoto_vrp_solver_CoESVRPJNI */
 
+static MDVRPProblem *problem = new MDVRPProblem();
+static AlgorithmConfig *config = new AlgorithmConfig();
+
+static jint vSize;
+static jfloat *jvXDepots;
+static jfloat *jvYDepots;
+static jfloat *jvDurations;
+static jint *jvCapacities;
+static jint cSize;
+static jfloat *jcXDepots;
+static jfloat *jcYDepots;
+static jint *jcDemands;
+
+static bool explicitC2CDistance = false;
+static typedef_vectorMatrix<float> customerDistances;
+static bool explicitD2CDistance = false;
+static typedef_vectorMatrix<float> depotDistances;
+
+static vector<int> depots;
+static vector<int> routes;
+static vector<float> costs;
+static vector<int> demands;
+static vector<vector<int>> customers;
+
 #ifndef _Included_com_soedomoto_vrp_solver_CoESVRPJNI
 #define _Included_com_soedomoto_vrp_solver_CoESVRPJNI
 #ifdef __cplusplus
@@ -25,11 +49,107 @@ JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_setCustomers
 
 /*
  * Class:     com_soedomoto_vrp_solver_CoESVRPJNI
+ * Method:    setCustomerToCustomerDistance
+ * Signature: (II[[F)V
+ */
+JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_setCustomerToCustomerDistance
+        (JNIEnv *, jobject, jint, jint, jobjectArray);
+
+/*
+ * Class:     com_soedomoto_vrp_solver_CoESVRPJNI
+ * Method:    setDepotToCustomerDistance
+ * Signature: (II[[F)V
+ */
+JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_setDepotToCustomerDistance
+        (JNIEnv *, jobject, jint, jint, jobjectArray);
+
+/*
+ * Class:     com_soedomoto_vrp_solver_CoESVRPJNI
+ * Method:    setDepotToDepotDistance
+ * Signature: (II[[F)V
+ */
+JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_setDepotToDepotDistance
+        (JNIEnv *, jobject, jint, jint, jobjectArray);
+
+/*
+ * Class:     com_soedomoto_vrp_solver_CoESVRPJNI
+ * Method:    configSetNumSubpopulation
+ * Signature: (I)V
+ */
+JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_configSetNumSubpopulation
+        (JNIEnv *, jobject, jint);
+
+/*
+ * Class:     com_soedomoto_vrp_solver_CoESVRPJNI
+ * Method:    configStopWhenMaxExecutionTime
+ * Signature: (F)V
+ */
+JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_configStopWhenMaxExecutionTime
+        (JNIEnv *, jobject, jfloat);
+
+/*
+ * Class:     com_soedomoto_vrp_solver_CoESVRPJNI
+ * Method:    configStopWhenMaxTimeWithoutUpdate
+ * Signature: (F)V
+ */
+JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_configStopWhenMaxTimeWithoutUpdate
+        (JNIEnv *, jobject, jfloat);
+
+/*
+ * Class:     com_soedomoto_vrp_solver_CoESVRPJNI
+ * Method:    configStopWhenNumGeneration
+ * Signature: (I)V
+ */
+JNIEXPORT void JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_configStopWhenNumGeneration
+        (JNIEnv *, jobject, jint);
+
+/*
+ * Class:     com_soedomoto_vrp_solver_CoESVRPJNI
  * Method:    solve
  * Signature: ()I
  */
 JNIEXPORT jint JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_solve
   (JNIEnv *, jobject);
+
+/*
+ * Class:     com_soedomoto_vrp_solver_CoESVRPJNI
+ * Method:    getSolutionDepots
+ * Signature: ()[I
+ */
+JNIEXPORT jintArray JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_getSolutionDepots
+        (JNIEnv *, jobject);
+
+/*
+ * Class:     com_soedomoto_vrp_solver_CoESVRPJNI
+ * Method:    getSolutionRoutes
+ * Signature: ()[I
+ */
+JNIEXPORT jintArray JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_getSolutionRoutes
+        (JNIEnv *, jobject);
+
+/*
+ * Class:     com_soedomoto_vrp_solver_CoESVRPJNI
+ * Method:    getSolutionCosts
+ * Signature: ()[F
+ */
+JNIEXPORT jfloatArray JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_getSolutionCosts
+        (JNIEnv *, jobject);
+
+/*
+ * Class:     com_soedomoto_vrp_solver_CoESVRPJNI
+ * Method:    getSolutionDemands
+ * Signature: ()[I
+ */
+JNIEXPORT jintArray JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_getSolutionDemands
+        (JNIEnv *, jobject);
+
+/*
+ * Class:     com_soedomoto_vrp_solver_CoESVRPJNI
+ * Method:    getSolutionCustomers
+ * Signature: ()[[I
+ */
+JNIEXPORT jobjectArray JNICALL Java_com_soedomoto_vrp_solver_CoESVRPJNI_getSolutionCustomers
+        (JNIEnv *, jobject);
 
 #ifdef __cplusplus
 }
